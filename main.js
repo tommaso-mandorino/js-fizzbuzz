a15e14b0444bbdf304b80ee458bbb825baccf17f
tommaso-mandorino <tommasomandorino@outlook.it>
2025-05-28 17:25:33 +0200

    feat: add 'Fizz' to resulting string if index is multiple of 3

--- main.js
+++ main.js
@@ -3,6 +3,14 @@ for (let i = 1; i <= 100; i++) {
 
     // Resulting string to print
     let finalString = '';
-    
+
+    // IF number is a multiple of 3
+    if (i % 3 == 0) {
+
+        // Add 'Fizz' to finalString
+        finalString += 'Fizz';
+
+    }
+
 }
 // #endregion For loop from 1 to 100
